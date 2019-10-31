ba187591bf8c0d8eaee6b073e9d513234e691200
LiliaRibac <ribac.lilia@gmai.com>
2019-10-31 15:26:37 -0700

    hashing password

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,10 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-var encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+var md5 = require('md5');
 
 const app = express();
-//  console.log(process.env.API_KEY)
+
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
@@ -26,10 +27,16 @@ const userSchema = new mongoose.Schema({
 });
 
 
+/*
+//mongoose-encryption password start
 userSchema.plugin(encrypt, {
   secret: process.env.SECRET,
   encryptedFields: ['password']
 });
+//mongoose-encryption password End
+*/
+
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -66,7 +73,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
 
   User.findOne({
