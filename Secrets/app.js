5f2ad5961e809420d5df03b5384b4a2300125eb4
LiliaRibac <ribac.lilia@gmai.com>
2019-10-31 14:27:31 -0700

    Add Environment Vars

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,26 +1,35 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
 
 const app = express();
+//  console.log(process.env.API_KEY)
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {
   useNewUrlParser: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ['password']
+});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -46,26 +55,28 @@ app.post("/register", function (req, res) {
     password: req.body.password
   });
 
-  newUser.save(function(err){
-    if(err){
+  newUser.save(function (err) {
+    if (err) {
       console.log(err)
-    }else{
+    } else {
       res.render("secrets")
     }
   });
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
 
-  User.findOne({email:username}, function(err, foundUser){
-    if(err){
+  User.findOne({
+    email: username
+  }, function (err, foundUser) {
+    if (err) {
       console.log(err)
-    }else{
-      if(foundUser){
-        if(foundUser.password ===password){
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
           res.render("secrets")
         }
       }
